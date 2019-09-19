4e352f2e6ab826c6764164d00fcd253da97cc248
r0zbot <r0zbot@users.noreply.github.com>
2019-09-19 15:34:26 -0300

    script now mounts

--- copy.sh
+++ copy.sh
@@ -3,13 +3,21 @@
 BASEDIR="$(dirname "$BASH_SOURCE")"
 cd $BASEDIR
 
-SD_BOOT=/run/media/r0zbot/BOOT/
-SD_ROOT=/run/media/r0zbot/SYSTEM/
+SD_BLK=/dev/sdc
+
+SD_BOOT=/mnt/labrador/boot
+SD_ROOT=/mnt/labrador/root
+
+mkdir -p /mnt/labrador/boot
+mkdir -p /mnt/labrador/root
+
+mount ${SD_BLK}1 $SD_BOOT
+mount ${SD_BLK}2 $SD_ROOT
 
 cp -rf data/labrador-linux/output/lib/modules $SD_ROOT/lib/
 cp -f data/labrador-linux/output/* $SD_BOOT
 
-sleep 1
+sleep 2
 
-umount -f SD_BOOT
-umount -f SD_ROOT
+umount -f $SD_BOOT
+umount -f $SD_ROOT
