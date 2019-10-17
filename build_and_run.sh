253b91ff3aab90ea54d3f3748763d8249ec168a4
r0zbot <r0zbot@users.noreply.github.com>
2019-10-17 17:47:42 -0300

    build and run

---
+++ build_and_run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+./build.sh
+ssh root@192.168.192.210 "rmmod labrador_eth || true"
+scp data/labrador-linux/linux/drivers/net/ethernet/caninos/labrador_eth.ko root@192.168.192.210:/usr/lib/modules/4.14.13/kernel/drivers/net/ethernet/caninos/labrador_eth.ko
+ssh root@192.168.192.210 "insmod /usr/lib/modules/4.14.13/kernel/drivers/net/ethernet/caninos/labrador_eth.ko" 
